364c68b7c80deaa9661c3de46ef74e5004190c2c
Stefan Forstenlechner <stefan.forstenlechner@tngtech.com>
2026-04-12 15:52:05 +0200

    Fix code style formatting (space before brace)

--- src/gui/GuiTools.h
+++ src/gui/GuiTools.h
@@ -39,7 +39,7 @@ class MouseWheelEventFilter : public QObject
 {
 public:
     explicit MouseWheelEventFilter(QObject* parent)
-        : QObject(parent){};
+        : QObject(parent) {};
 
 protected:
     bool eventFilter(QObject* obj, QEvent* event) override
